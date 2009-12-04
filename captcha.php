156387a7660a0617b8f7211cb4fb493b7265fc65
laconix <laconix.isnt@gmail.com>
2009-12-04 11:07:59 +1100

    improved generation (security)

--- captcha.php
+++ captcha.php
@@ -6,14 +6,15 @@ header('Content-type: image/png');
 $img = ImageCreate(100,20);
 $captcha = gettimeofday();
 $_SESSION['captcha'] = $captcha['usec'];
-$bg = imagecolorallocate($img, 41,49,52);
-$tc = imagecolorallocate($img, 70,85,85);
-$lc = imagecolorallocate($img, 100,80,80);
+$bg = imagecolorallocate($img, 255,255,255);
+$fc[1] = imagecolorallocate($img, 70,85,rand(30,120));
+$fc[2] = imagecolorallocate($img, 70,85,rand(30,120));
 $lines = rand(3,6);
 for ($i=1; $i <= $lines; $i++) {
-	imageline($img, rand(1,100),rand(1,20), rand(1,100),rand(1,20), $lc);
+	imageline($img, rand(1,100),rand(1,20), rand(1,100),rand(1,20), $fc[rand(1,2)]);
 }
-imagestring($img, 5, 22, 4, $captcha['usec'], $tc);
+imagestring($img, 5, 22, 4, substr($captcha['usec'],0,3), $fc[1]);
+imagestring($img, 5, 50, 4, substr($captcha['usec'],3), $fc[2]);
 
 imagepng($img);
 imagedestroy($img);
